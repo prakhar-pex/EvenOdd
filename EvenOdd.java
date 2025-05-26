c2486db404a7af01159c206f26d8eda0dbd62ec1
Prakhar <prakhar.1@bounteous.com>
2025-05-26 11:40:43 +0530

    EvenOdd

---
+++ EvenOdd.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        int[] arr = {10, 21, 4, 45, 66, 93, 1};
+
+        System.out.println("Even numbers:");
+        for (int num : arr) {
+            if (num % 2 == 0) {
+                System.out.print(num + " ");
+            }
+        }
+
+    System.out.println("\nOdd numbers:");
+        for (int num : arr) {
+            if (num % 2 != 0) {
+                System.out.print(num + " ");
+            }
+        }
+    }
+}
+
